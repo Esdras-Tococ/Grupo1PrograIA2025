ccf09da38c2f26fe793cef4535db7fa97aee546a
Jovany <ehernandezr50@miumg.edu.gt>
2025-03-12 13:34:53 -0600

    Se creo Paquete Zoologico con las clases Calculadora, Exportar y MenuZoo

---
+++ PROYECTO/src/main/java/com/mycompany/proyecto/Menu.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.proyecto;
+import com.mycompany.proyecto.Zoologico.Exportar;
+
+public class Menu {
+    public static void main(String[] args) {
+        Exportar.Export("leon, 1.60, 5kg, carnivoro");
+    }
+    
+}
