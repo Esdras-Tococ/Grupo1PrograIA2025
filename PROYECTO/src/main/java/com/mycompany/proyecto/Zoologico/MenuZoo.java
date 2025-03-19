0f11dbc965388a2cc842b13efca6e52d18d94a84
jayron <jayron@jayron>
2025-03-19 16:34:11 -0600

    se agregaron los metodos del menu:

--- PROYECTO/src/main/java/com/mycompany/proyecto/Zoologico/MenuZoo.java
+++ PROYECTO/src/main/java/com/mycompany/proyecto/Zoologico/MenuZoo.java
@@ -1,10 +1,79 @@
 
 package com.mycompany.proyecto.Zoologico;
 
+
 import java.util.Scanner;
 
 
+
+
 public class MenuZoo {
+  
+    public  void zoo() {
+    Scanner scanner =new  Scanner(System.in);
+         boolean salir =true;
+         int opcion;
+         while (salir){
+            System.out.println("=== Menú Zoo ===");
+            System.out.println("1. Registrar nuevo animal");
+            System.out.println("2. Listar todos los animales");
+            System.out.println("3. Alimentar a los animales");
+            System.out.println("4. Calcular el alimento consumido en un periodo de días");
+            System.out.println("5. Guardar datos en archivo CSV");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    registrarAnimales();
+
+                    break;
+                case 2:
+                    //listarAnimales();
+                    break;
+                case 3:
+                    //alimentarAnimales(scanner);
+                    break;
+                case 4:
+                    MenuCalculadora();
+                    break;
+                case 5:
+                  Registros b = new Registros();       
+                 b.Export();
+                 
+                    break;
+                case 6:
+                    System.out.println("Hasta pronto    ");
+                    System.out.println("regresando al menu principal");
+                    return;
+
+                default:
+                    System.out.println("Opción inválida. ingrese una opcion del menu.");
+                    break;
+            }
+        
+            }
+         
+        scanner.close();
+    }
+    
+    public  void registrarAnimales() {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Ingrese el nombre del animal: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Ingrese el tipo (mamífero, ave, reptil): ");
+        String tipo = scanner.nextLine();
+        System.out.print("Ingrese el tipo de dieta (herbívoro, carnívoro, omnívoro): ");
+        String dieta = scanner.nextLine();
+        System.out.print("Ingrese la cantidad de alimento por día en kg: ");
+        double cantidadAlimento = scanner.nextDouble();
+        scanner.nextLine();
+        
+    
+        
+    }
     
     
     
