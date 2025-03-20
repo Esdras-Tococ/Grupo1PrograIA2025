49a415a72aac775dd24f82b6d7cdeca794aed31f
Jovany <ehernandezr50@miumg.edu.gt>
2025-03-20 09:26:59 -0600

    Cambios en la clases Registros y Calculador

--- PROYECTO/src/main/java/com/mycompany/proyecto/Zoologico/Registros.java
+++ PROYECTO/src/main/java/com/mycompany/proyecto/Zoologico/Registros.java
@@ -5,25 +5,30 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Registros {
-    
+    private static boolean columnas = false;
     public static void Export() {
         String archivoCSV ="data.csv";
         
-        try (FileReader fr = new FileReader("Data.txt")) {
+        try (FileReader fr = new FileReader("Data.txt",StandardCharsets.UTF_8)) {
          BufferedReader br = new BufferedReader(fr);
       
          String linea;
          while((linea=br.readLine())!=null)
              
-            try (PrintWriter writer = new PrintWriter(new FileWriter(archivoCSV, true))) {
-            long ID = Files.lines(Paths.get(archivoCSV)).count();
-            writer.println(ID+","+linea);
+            try (PrintWriter writer = new PrintWriter(new FileWriter(archivoCSV,StandardCharsets.UTF_8,true))) {
+           
+                if (!columnas) { columnas = true;
+                 
+                    writer.println("ID, NOMBRE, PESO, AÑOS ,ALTURA, ALIAS, COMSUMO DE COMDIDA KG");}
+        
+            writer.println(linea);
         }   catch (IOException e) {
-            System.out.println("Error al escribir en el archivo");
+            System.out.println("Error al escribir en el archivo "+e.getMessage());
         }
       }
       catch(Exception e){
@@ -47,7 +52,7 @@ public class Registros {
      public static void save(String datos) {//recibe un argumento string 
         String archivoCSV="Data.txt";
         String datos7= datos.toLowerCase();
-        try (PrintWriter writer = new PrintWriter(new FileWriter(archivoCSV, true))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivoCSV,StandardCharsets.UTF_8, true))) {
         long ID = Files.lines(Paths.get(archivoCSV)).count();
             writer.println(ID+","+datos7);
         } catch (IOException e) {
