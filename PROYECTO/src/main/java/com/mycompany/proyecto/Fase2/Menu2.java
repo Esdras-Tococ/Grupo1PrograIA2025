d2f3e77dfe0b9032259b1e97b625a6dc6d6ba675
Esdras_morales <etococm@miumg.edu.gt>
2025-05-04 19:24:03 -0600

    Menu Fase 2

--- PROYECTO/src/main/java/com/mycompany/proyecto/Fase2/Menu2.java
+++ PROYECTO/src/main/java/com/mycompany/proyecto/Fase2/Menu2.java
@@ -1,21 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.proyecto.Fase2;
+import java.util.Scanner;
 import  com.mycompany.proyecto.Zoologico.MenuZoo;
+
+
 public class Menu2 {
-    
-    
-    
-    
-    public void menu(){
-   
-    
+    public static void main(String[] args) {
+          MenuZoo a = new MenuZoo();  
+         Scanner scanner= new Scanner(System.in);
+         boolean salir =true;
+         int opcion;
+         
+            
+        
+        while (salir){  
+            System.out.println("\nMenu:");
+            System.out.println("Elige una opcion:");
+            System.out.println("Opcion 1: Agregar Mamifero");
+            System.out.println("Opcion 2: Agregar Ave");
+            System.out.println("Opcion 3: Agregar Reptil");
+            System.out.println("Opcion 4: Ordenar Arreglo");
+            System.out.println("Opcion 5: Mostrar Arreglo");
+            System.out.println("Opcion 6: Salir");
+
+            opcion =scanner.nextInt();
+         
+            
+            try {
+                
+                 switch (opcion){
+               
+                case 1:
+                        System.out.println("Agregar Mamifero:");
+                    break;
+                case 2:
+                        System.out.println("Agregaar Ave:");
+                    break;
+                case 3:
+                        System.out.println("Agregar Reptil");
+                    break;    
+                 case 4:
+                        System.out.println("Ordenar Arreglo");
+                    break;
+                    case 5:
+                        System.out.println("Mostrar Arreglo");
+                            break;
+                    case 6:
+                        System.out.println("Saliendo de Menu");
+                            return;
+                default:
+                    System.out.println("opcion invalidad seleccione una opcion del menu:");
+                    break;
+                 }
+                
+            } catch (Exception e) {
+                System.err.println("ingrese una opcion validad:.");
+                System.err.println("Seleccione una opcion:");
+            }
+           
+        }
     }
-    
-    
-    
-    
-    
-}
+        
+    }
